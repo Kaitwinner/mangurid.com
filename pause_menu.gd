e2ef132df0ad410b68d943c3f580b07a4ec421e2
Kaitwinner <Kaitevander@gmail.com>
2025-04-19 17:26:30 +0300

    changes

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,11 +1,24 @@
 extends Control
 
-func _ready():
-	visible = false
-
-func _on_continue_button_down() -> void:
+func resume():
 	get_tree().paused = false
-	visible = false
 
-func _on_quit_button_down() -> void:
+func pause():
+	get_tree().paused = true
+
+func Esctest():
+	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		pause()
+
+func _on_continue_pressed() -> void:
+	resume()
+
+
+func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func toggle_pause():
+	get_tree().paused = !get_tree().paused
+	$CanvasLayer.visible = get_tree().paused
