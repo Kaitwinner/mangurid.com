5c44da1365490fa054cfec0833fb9533a8b12eaa
Fortuotu <103213729+Fortuotu@users.noreply.github.com>
2025-04-19 17:07:55 +0300

    More fixes

--- player.gd
+++ player.gd
@@ -3,9 +3,10 @@ class_name Player extends CharacterBody2D
 @export var speed = 400
 @export var gravity = 980
 
-@onready var gun_dist: float = global_position.distance_to($Gun.global_position)
+@onready var gun_dist = global_position.distance_to($Gun.global_position)
 var gun_force = 780.0
-var gun_ammo = 10
+const GUN_INITIAL_AMMO = 5
+var gun_ammo = GUN_INITIAL_AMMO
 
 func _get_input():
 	if Input.is_action_just_pressed("shoot"):
@@ -19,6 +20,10 @@ func _physics_process(delta):
 	_get_input()
 	_make_gun_rotation()
 	move_and_slide()
+	
+	if (is_on_floor() && gun_ammo != GUN_INITIAL_AMMO):
+		print("Gave player " + str(GUN_INITIAL_AMMO) + " ammo")
+		gun_ammo = GUN_INITIAL_AMMO
 
 func _make_gun_rotation():
 	var angle_vec = (get_global_mouse_position() - global_position).normalized()
